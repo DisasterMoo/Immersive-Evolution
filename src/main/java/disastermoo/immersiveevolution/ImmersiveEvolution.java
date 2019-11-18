a2b2227aedd11aea8e82c5f7b90da2e7067dc6b9
DisasterMoo <luan.santos.pinto@gmail.com>
2019-11-18 13:49:16 -0300

    A few more missing bits

--- src/main/java/disastermoo/immersiveevolution/ImmersiveEvolution.java
+++ src/main/java/disastermoo/immersiveevolution/ImmersiveEvolution.java
@@ -1,5 +1,6 @@
 package disastermoo.immersiveevolution;
 
+import org.apache.logging.log4j.Logger;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
@@ -7,17 +8,18 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
-import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ImmersiveEvolution.MODID, name = ImmersiveEvolution.NAME, version = ImmersiveEvolution.VERSION, dependencies = "required-after:immersiveengineering@[0.12,);", certificateFingerprint = ImmersiveEvolution.SIGNING_KEY)
+@SuppressWarnings("WeakerAccess")
+@Mod(modid = ImmersiveEvolution.MOD_ID, name = ImmersiveEvolution.NAME, version = ImmersiveEvolution.VERSION, dependencies = "required-after:immersiveengineering@[0.12,);", certificateFingerprint = ImmersiveEvolution.SIGNING_KEY)
 public class ImmersiveEvolution
 {
-    public static final String MODID = "immersiveevolution";
+    public static final String MOD_ID = "immersiveevolution";
     public static final String NAME = "Immersive Evolution";
     public static final String VERSION = "@VERSION@";
     public static final String SIGNING_KEY = "@FINGERPRINT@";
 
     private static Logger logger;
+    private static boolean signedBuild = true;
 
     private SimpleNetworkWrapper network;
 
@@ -42,7 +44,10 @@ public class ImmersiveEvolution
     @EventHandler
     public void onFingerprintViolation(FMLFingerprintViolationEvent event)
     {
-        logger.warn("Invalid fingerprint detected! This means this jar file has been modified externally and is not supported");
+        if (!event.isDirectory())
+        {
+            signedBuild = false;
+        }
     }
 
     @EventHandler
@@ -57,8 +62,12 @@ public class ImmersiveEvolution
     {
         logger = event.getModLog();
         //NetworkRegistry.INSTANCE.registerGuiHandler(this, new TechGuiHandler());
-        network = NetworkRegistry.INSTANCE.newSimpleChannel(MODID);
+        network = NetworkRegistry.INSTANCE.newSimpleChannel(MOD_ID);
         int id = 0;
         //network.registerMessage(new PacketTileEntityUpdate.Handler(), PacketTileEntityUpdate.class, ++id, Side.CLIENT);
+        if (!signedBuild)
+        {
+            logger.error("INVALID FINGERPRINT DETECTED! This means this jar file has been compromised and are not supported.");
+        }
     }
 }
